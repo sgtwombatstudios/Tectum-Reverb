60303ce9ac6efa29a167da35b659c71d9fd79120
Jonathan Gatten <jonathangatten@derek-mac.local>
2016-05-13 17:29:03 -0400

    Initial Commit

---
+++ Tectum Reverb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tectum Reverb
+//
+//  Created by Jonathan Gatten on 5/13/16.
+//  Copyright © 2016 Tectum Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tectum Reverb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tectum Reverb
+//
+//  Created by Jonathan Gatten on 5/13/16.
+//  Copyright © 2016 Tectum Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
